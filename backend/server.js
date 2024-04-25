4e31564979e41699451b9d3777a8660cd9dc46ed
ethan <ethanrdibble@gmail.com>
2024-04-25 10:11:53 -0700

    added socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
-import records from "./routes/record.js";
+//import records from "./routes/record.js";
 
-const PORT = process.env.PORT || 5050;
+import authRoutes from "./routes/auth.routes.js";
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+//app.use("/record", records);
+
+app.get("/", (req, res) => {
+	// root route http://localhost:5000/
+	res.send("Hello World");
+});
+
+app.use("api/auth", authRoutes)
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
 
