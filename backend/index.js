5fd96c2e59253ab7ded031a5ec180444a9bfa0ee
ethan <ethanrdibble@gmail.com>
2024-04-30 11:37:20 -0700

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,35 @@
-import path from "path";
-import { fileURLToPath } from 'url';
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { createServer } from "node:http";
-import { join } from "node:path";
-import { Server } from "socket.io";
-//import records from "./routes/record.js";
+require('dotenv').config();
 
-// import authRoutes from "./routes/auth.routes.js";
+//const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+//const records = require ('./routes/record.js');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// import authRoutes from './routes/auth.routes.js';
+
+// PORT
 const PORT = process.env.PORT || 5000;
+// express app
 const app = express();
+// express server
 const server = createServer(app);
+// socket io
 const io = new Server(server);
 
 app.use(cors());
-app.use(express.json());
-//app.use("/record", records);
 
-app.get("/", (req, res) => {
+// middleware
+app.use(express.json());
+//app.use('/record', records);
+
+app.get('/', (req, res) => {
 	// root route http://localhost:5000/
-	// res.send("Hello World");
+	// res.send('Hello World');
 	res.sendFile(join(__dirname, 'index.html'));
 });
 
@@ -30,10 +37,20 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-// app.use("api/auth", authRoutes)
+// routes
+// app.use('api/auth', authRoutes)
 
-// start the Express server
-server.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-});
+// connect to database
+mongoose.connect(process.env.URI)
+	.then(() => {
+		// start the Express server
+		server.listen(PORT, () => {
+			console.log(`Server listening on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error)
+	})
+
+
 
