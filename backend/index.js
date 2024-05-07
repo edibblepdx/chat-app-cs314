92ea8ae00683661d4f3f9f958a844573d5f0f3f2
ethan <ethanrdibble@gmail.com>
2024-05-07 12:14:52 -0700

    commenting

--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,59 @@
 require('dotenv').config();
 
-//const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
-const Message = require('./models/message');
-const chatRoutes = require('./routes/chats');
 
-// PORT
-const PORT = process.env.PORT || 5000;
-// express app
-const app = express();
-// express server
-const server = createServer(app);
-// socket io
-const io = new Server(server);
-// cross origin resource sharing
-app.use(cors());
+const Message = require('./models/message');
+const chatRoutes = require('./routes/chat.routes');
+
+const PORT = process.env.PORT || 5000;	// PORT
+const app = express();					// express app
+const server = createServer(app);		// express server
+const io = new Server(server);			// socket.io
 
 // middleware
+app.use(cors());
 app.use(express.json());
+
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
 	next();
 })
 
+// chat routes
 app.use('/chats', chatRoutes);
 
-/*
-app.get('/', (req, res) => {
-	// root route http://localhost:5000/
-	// res.send('Hello World');
-	res.sendFile(join(__dirname, 'index.html'));
-});
-*/
-
+// socket.io
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
-	console.log('message: ' + msg);	
-  	io.emit('chat message', msg);
-	// save message document to database
-	try {
-		const message = Message({message: msg});
-		message.save();
-	} catch (err) {
-		console.log(err.message)	
-	}
-  });
-  socket.on('disconnect', () => {
-	console.log('user disconnected');
-  });
+	// user connect
+  	console.log('a user connected');
+	// on a message run a callback function to...
+	socket.on('chat message', (msg) => {
+		// print the message to the console
+		console.log('message: ' + msg);	
+		// emit the message to all users
+		io.emit('chat message', msg);
+		try {
+			// save the message to the database
+			const message = Message({message: msg});
+			message.save();
+		} catch (err) {
+			console.log(err.message)	
+		}
+	});
+	// user disconnect
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
 });
 
 // connect to database
 mongoose.connect(process.env.URI)
 	.then(() => {
+		console.log('connected to database')
 		// start the Express server
 		server.listen(PORT, () => {
 			console.log(`Server listening on port ${PORT}`);
@@ -66,7 +61,6 @@ mongoose.connect(process.env.URI)
 	})
 	.catch((err) => {
 		console.log(err)
-	})
-
+	});
 
 
