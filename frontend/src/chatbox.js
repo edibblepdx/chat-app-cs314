ab587a55c39f6ba304c05253ede191babd0e521a
Andres Gutierrez Bravo <andresgutierrezbravo@Andress-MacBook-Air.local>
2024-05-23 21:06:29 -0700

    Support for scrollable screen and chat bubbles

--- frontend/src/chatbox.js
+++ frontend/src/chatbox.js
@@ -1,4 +1,10 @@
+
+//Component for the entire chat box
 const Chatbox = () => {
+
+
+    let test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    //Input box component
     const customInput = () => {
         return (
             <div className="inputBox">
@@ -7,6 +13,7 @@ const Chatbox = () => {
         );
     }
 
+    //Send Button Component to send messages
     const sendButton = () => {
         return (
             <div>
@@ -15,22 +22,34 @@ const Chatbox = () => {
                     textDecoration: "none",
                     position: "relative",
                     top: "640px",
-                    left: "920px",
+                    left: "980px",
                     color: "White",
                     borderRadius: "5px",
                     borderWidth: "4px",
-                    padding: "15px",
+                    padding: "15.5px",
                     display: "flex",
                 }}>Send</a>
             </div>
         );
     }
 
+    //Chat Bubbles ***works***
+    const chatBubble = () => {
+        return (
+            <div className="customBub">
+                {test}
+            </div>
+        );
+    }
+
+
     return (
         <div className="chatBox">
-            Chat Box
             {customInput()}
             {sendButton()}
+            <div className="scrollChat">
+
+            </div>
         </div>
     );
 };
