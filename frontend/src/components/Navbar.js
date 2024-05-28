3c6abf98bd519853233cead33f1499a4ff61d97d
Andres Gutierrez Bravo <andresgutierrezbravo@Andress-MacBook-Air.local>
2024-05-27 20:28:43 -0700

    Styled login and register pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,22 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/chats'>Chats</Link>
-      <Link to='/user/login'>Login</Link>
-      <Link to='/user/register'>Register</Link>
+      <Link to='/' style={{
+        textDecoration: "none"
+      }}>Home</Link>
+
+      <Link to='/chats' style={{
+        textDecoration: "none"
+      }}>Chats</Link>
+
+      <Link to='/user/login' style={{
+        textDecoration: "none"
+      }}>Login</Link>
+
+      <Link to='/user/register' style={{
+        textDecoration: "none"
+      }}>Register</Link>
+
     </nav>
   )
 }
